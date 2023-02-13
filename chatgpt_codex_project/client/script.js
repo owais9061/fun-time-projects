c1fe81aa4f73c770bd4532881cc96bb4f47f9d93
owais9061 <owaischaudhry906@gmail.com>
2023-02-13 19:04:05 +0500

    Done half logic

--- chatgpt_codex_project/client/script.js
+++ chatgpt_codex_project/client/script.js
@@ -1,23 +1,34 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+import bot from './assets/bot.svg';
+import user from './assets/user.svg';
+const from = document.querySelector('input');
+const chatcontainer = document.querySelector('#chat_container');
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+let loadInterval;
 
-setupCounter(document.querySelector('#counter'))
+function loader(element) {
+  element.textContent = '';
+
+  loadInterval = setInterval(()=> {
+    element.textContent+='.';
+    if(element.textContent === '....'){
+      element.textContent='';
+    }
+  },300)
+}
+
+function typeText(element, text){
+  let index =0;
+  let interval = setInterval(()=> {
+    if(index < text.length){
+      element.innerHTML += text.charAt(index);
+    }
+    else{clearInterval(interval)};
+  },20)
+}
+
+function generateUniqueId(){
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
+  return `id-${timestamp}-${hexadecimalString}`;
+}
