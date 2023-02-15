5bca22e018cec79f925cc4a875cdbb6847d2d5f4
owais9061 <owaischaudhry906@gmail.com>
2023-02-15 13:12:56 +0500

    Done the necessay Styling and JS changes (with comments)

--- binary-to-decimal-converter/app.js
+++ binary-to-decimal-converter/app.js
@@ -1,14 +1,23 @@
+//! Select the container 
+
 const container = document.querySelector('.container');
+
+//! Add the after click function to the button using callback method
 
 container.addEventListener('submit', (event)=>{
 
+
     event.preventDefault();
+
+//! Variabes initializer 
 
     let decimal = 0;
     let counter = 0;
 
-    const result = document.querySelector('.result');
-    let binary = document.querySelector('#binary').value;
+//! THe formulas
+
+    const result = document.querySelector('.result'); //! box where the result will b displayed
+    let binary = document.querySelector('#binary').value; //! box where the binary value is inputted
 
     while(binary > 0){
         let lastDigit = Math.round(binary % 10);
@@ -17,5 +26,5 @@ container.addEventListener('submit', (event)=>{
         binary /= 10;
     }
 
-    result.textContent = `Decimal Number : ${decimal}`;
-});
+    result.textContent = `Decimal Number : ${decimal}`; //! here Decimal number is the template string (becuase we use the variable decimal so the template string will be used.)
+}); //! end if entire callback function for event listener
